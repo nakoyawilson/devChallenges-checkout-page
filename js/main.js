e7e2051dfc71afa811059367b8d1b7cd3edd9670
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-04 23:24:36 -0400

    Add responsiveness and functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 const country = document.querySelector("#country");
+const totalCost = document.querySelector("#total-cost");
+const shippingCost = document.querySelector("#shipping-cost");
+const costBag = 54.99;
+const costShoe = 74.99;
+const shipping = 19;
+const decreaseBag = document.querySelector("#decrease-bag");
+const increaseBag = document.querySelector("#increase-bag");
+const decreaseShoe = document.querySelector("#decrease-shoe");
+const increaseShoe = document.querySelector("#increase-shoe");
+const quantityBag = document.querySelector("#quantity-bag");
+const quantityShoe = document.querySelector("#quantity-shoe");
+const submitButton = document.querySelector(".submit-btn");
+const inputs = document.querySelector("input");
+
+const calculateTotal = () => {
+  const bagQty = Number(quantityBag.innerHTML);
+  const shoeQty = Number(quantityShoe.innerHTML);
+  const total = bagQty * costBag + shoeQty * costShoe + shipping;
+  if (bagQty === 0 && shoeQty === 0) {
+    totalCost.innerHTML = "0.00";
+    shippingCost.innerHTML = "0";
+  } else {
+    totalCost.innerHTML = total.toFixed(2);
+    shippingCost.innerHTML = "19";
+  }
+};
 
 country.addEventListener("change", () => {
   if (country.value !== "") {
@@ -6,4 +32,60 @@ country.addEventListener("change", () => {
   } else {
     country.style.color = "#828282";
   }
+});
+
+decreaseBag.addEventListener("click", () => {
+  let count = Number(quantityBag.innerHTML);
+  if (count > 0) {
+    count--;
+  }
+  if (count === 0) {
+    quantityBag.style.color = "#bdbdbd";
+  }
+  quantityBag.innerHTML = count;
+  calculateTotal();
+});
+
+increaseBag.addEventListener("click", () => {
+  let count = Number(quantityBag.innerHTML);
+  count++;
+  if (count > 0) {
+    quantityBag.style.color = "#333333";
+  }
+  quantityBag.innerHTML = count;
+  calculateTotal();
+});
+
+decreaseShoe.addEventListener("click", () => {
+  let count = Number(quantityShoe.innerHTML);
+  if (count > 0) {
+    count--;
+  }
+  if (count === 0) {
+    quantityShoe.style.color = "#bdbdbd";
+  }
+  quantityShoe.innerHTML = count;
+  calculateTotal();
+});
+
+increaseShoe.addEventListener("click", () => {
+  let count = Number(quantityShoe.innerHTML);
+  count++;
+  if (count > 0) {
+    quantityShoe.style.color = "#333333";
+  }
+  quantityShoe.innerHTML = count;
+  calculateTotal();
+});
+
+submitButton.addEventListener("click", (e) => {
+  alert("Success! Your order has been placed.");
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+      submitButton.click();
+    }
+  });
 });
